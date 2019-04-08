9cfbed4c34086b8f051f4aae68c7689004e683b3
sergeevev-git <sergeevev@gmail.com>
2019-04-08 21:20:00 +0500

    for 2nd lab commit

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-'use strict';
+let money = prompt("Каков ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
- let money = +prompt("Каков ваш бюджет на месяц?", "0");
-   console.log(money);
-let time = prompt("Введите дату в формате YYYY-MM-DD", "2019-01-01");
-   console.log(time);
-
-let  persone = {
+let persone = {
     budget: money,
     timeData: time,
-    expenses : {
-        mustBuy: "",
-        price: ""
-    },
-    optionalExpenses : {
-
-    },
+    expenses : {},
+    optionalExpenses : {},
     income : [],
     savings : false
 };
 
-persone.expenses.mustBuy = prompt("Введите обязательную статью расходов в этом месяце", "");
-    console.log(persone.expenses.mustBuy);
-persone.expenses.price = +prompt("Во сколько обойдется?", "0");
-    console.log(persone.expenses.price);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+        if (typeof(a)==='string'){ 
+
+           persone.expenses[a] = b;
+        }
+}
 
 alert("Бюджет на один день: " + persone.budget/30);
-    console.log(persone.budget/30);
