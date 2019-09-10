9304c7da8a1f8d3f6a88f20340a9c5192014c451
sergeevev-git <sergeevev@gmail.com>
2019-09-10 14:52:42 +0500

    получение элементов страницы

---
+++ budget-app/src/js/main.js
@@ -0,0 +1,55 @@
+let btn_begin = document.getElementById('start');
+
+let budget_value = document.getElementsByClassName('budget-value');
+let daybudget_value = document.getElementsByClassName('daybudget-value');
+let level_value = document.getElementsByClassName('level-value');
+let expenses_value = document.getElementsByClassName('expenses-value');
+let optionalexpenses_value = document.getElementsByClassName('optionalexpenses-value');
+let income_value = document.getElementsByClassName('income-value');
+let monthsavings_value = document.getElementsByClassName('monthsavings-value');
+let yearsavings_value = document.getElementsByClassName('yearsavings-value');
+
+let expenses_item = document.getElementsByClassName('expenses-item');
+
+let expenses_item_btn = document.getElementsByTagName('button')[0];
+let optionalexpenses_btn = document.getElementsByTagName('button')[1];
+let count_budget_btn = document.getElementsByTagName('button')[2];
+
+let optionalexpenses_item = document.querySelectorAll('.optionalexpenses-item');
+
+let choose_income = document.querySelector('.choose-income');
+let savings = document.querySelector('#savings');
+let choose_sum = document.querySelector('.choose-sum');
+let choose_percent = document.querySelector('.choose-percent');
+let year_value = document.querySelector('.year-value');
+let month_value = document.querySelector('.month-value');
+let day_value = document.querySelector('.day-value');
+
+
+//let result_divs = document.querySelectorAll(".result-table .-value");
+// ищем по классу внутри form
+//let target_divs = result_divs.getElementsByClassName('-value');
+  //alert(articles.length);
+
+//document.result_divs.getElementsByClassName('-value');
+
+
+
+
+console.log(btn_begin);
+
+console.log(expenses_item);
+
+console.log(expenses_item_btn);
+console.log(optionalexpenses_btn);
+console.log(count_budget_btn);
+
+console.log(optionalexpenses_item);
+
+console.log(choose_income);
+console.log(savings);
+console.log(choose_sum);
+console.log(choose_percent);
+console.log(year_value);
+console.log(month_value);
+console.log(day_value);
