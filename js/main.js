652301cc09bbecb9bca0c1e73ef7a4b2d5e5477d
sergeevev-git <sergeevev@gmail.com>
2019-09-11 18:19:18 +0500

    реорганизоваол хранилище: 	- примеры и уроки сбросил в папку lessons 	- проджект вынес в корень папки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 'use strict';
+
+let btn_begin = document.getElementById('start');
+
+let budget_value = document.getElementsByClassName('budget-value');
+let daybudget_value = document.getElementsByClassName('daybudget-value');
+let level_value = document.getElementsByClassName('level-value');
+let expenses_value = document.getElementsByClassName('expenses-value');
+let optionalexpenses_value = document.getElementsByClassName('optionalexpenses-value');
+let income_value = document.getElementsByClassName('income-value');
+let monthsavings_value = document.getElementsByClassName('monthsavings-value');
+let yearsavings_value = document.getElementsByClassName('yearsavings-value');
+
+let expenses_item = document.getElementsByClassName('expenses-item');
+
+let expenses_item_btn = document.getElementsByTagName('button')[0];
+let optionalexpenses_btn = document.getElementsByTagName('button')[1];
+let count_budget_btn = document.getElementsByTagName('button')[2];
+
+let optionalexpenses_item = document.querySelectorAll('.optionalexpenses-item');
+
+let choose_income = document.querySelector('.choose-income');
+let savings = document.querySelector('#savings');
+let choose_sum = document.querySelector('.choose-sum');
+let choose_percent = document.querySelector('.choose-percent');
+let year_value = document.querySelector('.year-value');
+let month_value = document.querySelector('.month-value');
+let day_value = document.querySelector('.day-value');
+
 let money, time;
 
 function start () {
